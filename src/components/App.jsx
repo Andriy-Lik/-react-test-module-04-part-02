96d81f1191e751e11a11351bcd1d283936aa6525
Andriy-Lik <alan321987@gmail.com>
2023-03-16 15:08:29 +0200

    add stickers

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,50 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
+import { Layout } from './Layout';
+import { StickerForm } from './StickerForm/StickerForm';
+import { StickerList } from './StickerList/StickerList';
+import initialStickers from '../stickers.json';
+
+const getInitialStickers = () => {
+  const savedStickers = localStorage.getItem('stickers');
+  if (savedStickers !== null) {
+    const parsedStickers = JSON.parse(savedStickers);
+    return parsedStickers;
+  } else {
+    return initialStickers;
+  }
 };
+
+export const App = () => {
+  const [stickers, setStickers] = useState(getInitialStickers);
+
+  const addSticker = (img, label) => {
+    setStickers(pS => [...pS, { id: nanoid(), img, label },
+    ]);
+  };
+
+  const deleteSticker = stickerId => {
+    setStickers(pS => pS.filter(sticker => sticker.id !== stickerId));
+  };
+
+  // useEffect(() => {
+  //   const savedStickers = localStorage.getItem('stickers');
+  //   if (savedStickers !== null) {
+  //     const parsedStickers = JSON.parse(savedStickers);
+  //     setStickers(parsedStickers);
+  //   } else {
+  //     setStickers(initialStickers);
+  //   }
+  // }, []);
+
+  useEffect(() => {
+    localStorage.setItem('stickers', JSON.stringify(stickers));
+  }, [stickers]);
+
+  return (
+    <Layout>
+      <StickerForm onSubmit={addSticker} />
+      <StickerList items={stickers} onDelete={deleteSticker} />
+    </Layout>
+  );
+}
